a5bff6f4fbc460975de9843a7a9452ceb52f68d8
geoburdin <62799944+geoburdin@users.noreply.github.com>
2025-06-23 00:15:56 +0200

    8912

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-set -e            # exit on any error
-# launch API in background
+set -e
 uvicorn main:app --host 0.0.0.0 --port 8910 &
 # launch Streamlit in foreground (keeps container alive)
 streamlit run streamlit_app.py \
           --server.address 0.0.0.0 \
-          --server.port 8501
+          --server.port 8912
