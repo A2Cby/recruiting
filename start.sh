671550811298f284c1cfffb99bd2074085dffd04
geoburdin <62799944+geoburdin@users.noreply.github.com>
2025-06-23 00:24:14 +0200

    8912

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 set -e
 uvicorn main:app --host 0.0.0.0 --port 8910 &
 # launch Streamlit in foreground (keeps container alive)
-streamlit run streamlit_app.py \
+streamlit run website_demo.py \
           --server.address 0.0.0.0 \
           --server.port 8912
